8a833cda0640b6db4b84f22f14af8dda0a120985
Andrew Laminen <lampinen@stanford.edu>
2019-04-24 15:53:41 -0700

    parameters for main runs

--- meta_learning.py
+++ meta_learning.py
@@ -54,8 +54,8 @@ config = {
     "refresh_meta_cache_every": 1, # how many epochs between updates to meta_cache
     "refresh_mem_buffs_every": 50, # how many epochs between updates to buffers
 
-    "max_base_epochs": 0,#3000,
-    "max_new_epochs": 0,#100,
+    "max_base_epochs": 3000,
+    "max_new_epochs": 100,
     "num_task_hidden_layers": 3,
     "num_hyper_hidden_layers": 3,
     "train_drop_prob": 0.00, # dropout probability, applied on meta and hyper
@@ -67,7 +67,7 @@ config = {
                                    # hyper weights that generate the task
                                    # parameters. 
 
-    "output_dir": "/mnt/fs2/lampinen/polynomials/newer_results/untrained_baseline/",
+    "output_dir": "/mnt/fs2/lampinen/polynomials/newer_results/language/",
     "save_every": 20, 
     "sweep_meta_batch_sizes": [10, 20, 30, 50, 100, 200, 400, 800], # if not None,
                                                                     # eval each at
