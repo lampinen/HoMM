1153c048a52dccc4e37b69e80fded5d40ba05033
Andrew Lampinen <lampinen@stanford.edu>
2019-11-15 11:51:02 -0800

    Commenting a bit

--- HoMM/configs/default_architecture_config.py
+++ HoMM/configs/default_architecture_config.py
@@ -1,39 +1,44 @@
 import tensorflow as tf
 
 default_architecture_config = {
-   "input_shape": [4],
-   "output_shape": [1],
+   "input_shape": [4],  # dimension(s) of base input
+   "output_shape": [1],  # dimension(s) of base output
 
    "outcome_shape": None,  # If not None, allows outcomes != targets, for RL etc.
    "output_masking": False,  # If True, enables output masking, for RL tasks
 
-   "IO_num_hidden": 128,
+   "IO_num_hidden": 128,  # number of hidden units in input/output nets.
 
-   "z_dim": 512,
+   "z_dim": 512,  # latent dimensionality
 
-   "M_num_hidden_layers": 3,
-   "M_num_hidden": 512,
+   "M_num_hidden_layers": 3,  # meta-network hidden layers
+   "M_num_hidden": 512,  # meta-network hidden units
    "M_max_pool": True,  # if False, average pools.
 
-   "H_num_hidden_layers": 3,
-   "H_num_hidden": 512,
-   "task_weight_weight_mult": 1.0,
+   "H_num_hidden_layers": 3,  # hyper network hidden layers
+   "H_num_hidden": 512,  # hyper network hidden units
+   "task_weight_weight_mult": 1.0,  # weight scale init for hyper network to
+                                    # approximately preserve variance in task
+                                    # network
 
-   "F_num_hidden_layers": 3,
-   "F_num_hidden": 64,
+   "F_num_hidden_layers": 3,  # task network num hidden layers
+   "F_num_hidden": 64,  # task network hidden units
 
-   "internal_nonlinearity": tf.nn.leaky_relu, 
-   "output_nonlinearity": None,
+   "internal_nonlinearity": tf.nn.leaky_relu,  # nonlinearity for hidden layers
+                                               # -- note that output to Z is
+                                               # linear by default
 
-   "persistent_task_reps": False,
+   "persistent_task_reps": False,  # enable persistent task representations
+                                   # -- not included in original paper, but
+                                   # can be useful for learning complex tasks
 
-   "lang_drop_prob": 0.0,
-   "train_drop_prob": 0.0,
+   "train_drop_prob": 0.0,  # drop prob for M/H/etc
 
-   "num_lstm_layers": 2,
+   "lang_drop_prob": 0.0,  # drop prob for language processing
+   "num_lstm_layers": 2,  # for language processing
+   "max_sentence_len": 20,  # for language
 
-   "optimizer": "Adam",
-   "max_sentence_len": 20,
-   "memory_buffer_size": 1024,
-   "meta_batch_size": 50, # how many points the meta-learner sees
+   "optimizer": "Adam",  # Adam or RMSProp are supported options
+   "memory_buffer_size": 1024,  # how many data points to remember (per task)
+   "meta_batch_size": 50, # how many points the meta-learner sees each step
 }
--- HoMM/configs/default_run_config.py
+++ HoMM/configs/default_run_config.py
@@ -1,23 +1,23 @@
 default_run_config = {
-    "train_language": False,
-    "train_meta": True,
+    "train_language": False,  # whether to train language
+    "train_meta": True,  # whether to train meta class/map tasks, or just base
     
     "num_epochs": 10000,  # number of training epochs
     "refresh_mem_buffs_every": 50, # how many epochs between updates to buffers
     "eval_every": 20,  # how many epochs between evals
 
-    "num_runs": 5,
+    "num_runs": 5, # number of independent runs of the network to complete
 
-    "init_learning_rate": 3e-5,
-    "init_language_learning_rate": 3e-5,
-    "init_meta_learning_rate": 1e-5,
+    "init_learning_rate": 3e-5,  # initial learning rate for base tasks
+    "init_language_learning_rate": 3e-5,  # for language-cued tasks
+    "init_meta_learning_rate": 1e-5,  # for meta-classification and mappings
 
-    "lr_decay": 0.85,
-    "language_lr_decay": 0.8,
+    "lr_decay": 0.85,  # how fast base task lr decays (multiplicative)
+    "language_lr_decay": 0.8, 
     "meta_lr_decay": 0.85,
-    "lr_decays_every": 100,
+    "lr_decays_every": 100,  # lr decays happen once per this many epochs
 
-    "min_learning_rate": 3e-8,
+    "min_learning_rate": 3e-8,  # can't decay past these minimum values 
     "min_language_learning_rate": 3e-8,
     "min_meta_learning_rate": 1e-7,
 }
